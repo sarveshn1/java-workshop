003b673a9c631d8c58c258bb1e90c3247cd4bb3a
sarveshn1 <sarveshn24it@vetias.ac.in>
2025-07-02 14:27:01 +0530

    new code

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ in java class what we learn
 introdution to java
 java tool
 data type
+operators
+keywords
+control statement
+jar
+github login
+how to use notepad
---
+++ src/com/vetias/java/workshop/basics/Greetings.java
@@ -0,0 +1,7 @@
+package com.vetias.java.workshop.basics;
+
+public class Greetings {
+  public static void main(String[]args){
+    System.out.println("welcome to java");
+	}
+}
